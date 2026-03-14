2aad620a15110498698a5b94115133687fef9778
shale <slucas001@citymail.cuny.edu>
2026-03-14 19:11:24 -0400

    setup the initial structure of the events endpoint

--- backend/routers/events.py
+++ backend/routers/events.py
@@ -1,1 +1,31 @@
-# Event related stuff goes here
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from fastapi import APIRouter, HTTPException, Query
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+load_dotenv()
+
+router = APIRouter()
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.environ.get("SUPABASE_SERVICE_KEY") 
+
+if not (SUPABASE_URL or SUPABASE_SERVICE_KEY):
+	raise RuntimeError ("Missing Supbase URL or Service Key in .env file")
+
+supabase: Client = create_client (SUPABASE_URL, SUPABASE_SERVICE_KEY)
+
+
+class EventCreate(BaseModel): 
+	pass
+
+
+class EventUpdate(BaseModel):
+	pass
+
+class AttendeeAdd(BaseModel):
+	pass
+
+
