a30e44acd9fd9ac601fe364689b3bfd6853095ec
shale <slucas001@citymail.cuny.edu>
2026-03-14 16:15:49 -0400

    seed users with basic info

--- backend/seed.py
+++ backend/seed.py
@@ -18,7 +18,36 @@ if not (SUPABASE_URL or SUPABASE_SERVICE_KEY):
 supabase: Client = create_client (SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 def seed_users (count: int) -> list[str]:
-	pass
+	print(f"Creating {count} fake (realistic) users")
+
+	user_ids = []
+
+	for _ in range (count): 
+		email = fake.unique.email()
+		result = supbase.auth.admin.create_user (
+			{
+				"email": email, 
+				"password": "morganstanleyhackathon", 
+				"email_confirm": True,
+				"user_metadata" : {
+					"display_name": fake.name(),
+				}, 
+			} 
+		) 
+
+		uid = result.user.id
+		user_ids.append(uid)
+
+		supabase.table("profiles").update (
+			{ 
+				"display_name": fake.name(),	
+				"bio": fake.sentence(nb_words=15),
+			} 
+		).eq("id", uid).execute()
+
+		print (f" User -> [{email}, {uid}]")
+		return user_ids
+
 
 def seed_events (user_ids: list[str], count: int) -> list[str]:
 	pass
