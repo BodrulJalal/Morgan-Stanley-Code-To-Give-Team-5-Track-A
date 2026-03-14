3e4620d257a1a16d4b1ad6f32b7bc306a1705eed
shale <slucas001@citymail.cuny.edu>
2026-03-14 15:47:08 -0400

    connected to the db and executed simple query

--- backend/seed.py
+++ backend/seed.py
@@ -1,1 +1,20 @@
-print ('will do some db seeding')
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.environ.get("SUPABASE_SERVICE_KEY") 
+
+if not (SUPABASE_URL or SUPABASE_SERVICE_KEY):
+	raise RuntimeError ("Missing Supbase URL or Service Key in .env file")
+
+supabase: Client = create_client (SUPABASE_URL, SUPABASE_SERVICE_KEY)
+
+try: 
+	results = supabase.table("profiles").select("*").limit(1).execute()
+	print(results.data)
+
+except Exception as e: 
+	print(f"Failed to connect to Supabase: {e}")
