b87355d4dad5ebbb70588dce4b32d408da9264da
shale <slucas001@citymail.cuny.edu>
2026-03-15 15:02:18 -0400

    have a first impl for the connection manager

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -24,3 +24,23 @@ supabase: Client = create_client (SUPABASE_URL, SUPABASE_SERVICE_KEY)
 class ConnectionManager: 
 	def __init__ (self): 
 		self.rooms: dict[str, list[WebSocket]] = {}
+
+	async def connect (self, room_id: str, ws: WebSocket):
+		await ws.accept()
+
+		if room_id not in self.rooms:
+			self.rooms[room_id] = []
+		
+		self.rooms[room_id].append(ws)
+
+	def disconnect (self, room_id: str, ws: WebSocket):
+		self.rooms[room_id].remove(ws)
+
+	async def broadcast (self, room_id: str, message: dict):
+		for conn in self.rooms.get(room_id, []):
+			await conn.send_json(message)
+
+
+conn_manager = ConnectionManager()
+
+	
